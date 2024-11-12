1d58a50e7a72a09acac14576185abe3ad7fee7a5
sajib689 <jibonraja11@gmail.com>
2024-11-12 12:08:52 +0600

    get users route add success

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,8 @@
-import { createUser } from "./user.controler";
+import { createUser, getUsers } from "./user.controler";
 import express from "express";
 const router = express.Router();
 
 router.post('/create-users', createUser)
+router.get('/users', getUsers)
 
 export default router
