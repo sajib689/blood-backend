38374c39cfe145e24c8ba4ed7b4d50e2ba88d3be
sajib689 <jibonraja11@gmail.com>
2024-11-12 12:16:37 +0600

    add the contact route on the app file

--- app.ts
+++ app.ts
@@ -2,9 +2,11 @@ import express, { Application } from 'express'
 import cors from 'cors';
 const app:Application = express()
 import userRoute from './src/app/modules/user/user.route'
+import contactRoute from './src/app/modules/contact/contact.route'
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/user', userRoute)
+app.use("/api/v1/contact", contactRoute)
 
 export default app
